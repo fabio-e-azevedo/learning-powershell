439cb1405e5944c6128b23032fb9b1f9e44c7d20
Fabio Azevedo <emaildofabioeduardo@gmail.com>
2025-10-23 11:40:41 -0300

    where it all began...

---
+++ list-all-installed-software.ps1
@@ -0,0 +1,5 @@
+Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
+Where-Object { $_.DisplayName } |
+Select-Object DisplayName, DisplayVersion, Publisher, InstallDate |
+Sort-Object DisplayName
+
